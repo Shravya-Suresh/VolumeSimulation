74d87abef521ffe76d504dfc93c89c08fa7b2907
MAC <mac@MacBook-Pro.local>
2025-07-29 18:04:58 +0530

    Add subtract function

--- git-trails.py
+++ git-trails.py
@@ -2,7 +2,13 @@ def adding (a, b):
     res = a + b
     return res
 
+def subtract (a, b):
+    res = a - b
+    return res
+
 a = int(input("enter a number to add"))
 b = int(input("enter another number to add"))
-res = adding(a, b)
-print(res)
+res1 = adding(a, b)
+res2 = subtract(a, b)
+print("sum: " + res1)
+print("difference: "+ res2)
